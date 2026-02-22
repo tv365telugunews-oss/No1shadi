eaf98bf1252beb745f0883d50958e6f96f4616e5
tv365telugunews-oss <tv365telugunews@gmail.com>
2026-02-22 19:54:43 +0530

    Fixed merge conflicts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,9 +2,5 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes";
 
 export default function App() {
-<<<<<<< HEAD
-  return <RouterProvider router={router} future={{ v7_startTransition: true }} />;
-=======
   return <RouterProvider router={router} />;
->>>>>>> ad230b081e0a7eee68afbde3f69aebfe9320f883
 }
